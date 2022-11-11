472eee17e28568c0baff8435c4c152d8f77af14e
alvarofmk <alvarofmk@gmail.com>
2022-11-11 11:33:06 +0100

    mapas hechos

--- src/app/components/station-list/station-list.component.ts
+++ src/app/components/station-list/station-list.component.ts
@@ -23,10 +23,17 @@ export class StationListComponent implements OnInit {
   orderedByLesser: boolean = true;
   minPriceToShow: number = 0;
   maxPriceToShow: number = 5.00;
+  loading: boolean = true;
+  userLat: number = 0;
+  userLng: number = 0;
+  geoLocalized: boolean = false;
 
   constructor(private stationService: StationService) { }
 
   ngOnInit(): void {
+    this.getLocation();
+    console.log(this.userLat);
+    console.log(this.userLng);
     this.stationService.getProvinces().subscribe((response: Province[]) => {
       response.forEach(province => this.provinces.push(province))});
     this.stationService.getAllStations().subscribe((response: StationPricesResponse) => {
@@ -34,11 +41,14 @@ export class StationListComponent implements OnInit {
       this.stationPrices = response;
       this.stations = response.ListaEESSPrecio.filter(station => station[this.price] != '')
       this.sort();
+      this.loading = false;
     })
     
   }
 
   changePicked(){
+    debugger;
+    this.loading = true;
     switch (this.pricePicked) {
       case 1:
         this.price = 'Precio Biodiesel';
@@ -121,8 +131,19 @@ export class StationListComponent implements OnInit {
     });
   }
 
+  sortByClosest(){
+    this.stations.sort((station1, station2) => {
+      if(this.calculateDistance(station1) > this.calculateDistance(station2)){
+        return 1;
+      }else if(this.calculateDistance(station1) < this.calculateDistance(station2)){
+        return -1;
+      }else{
+        return 0;
+      }
+    });
+  }
+
   reFilter(){
-    debugger;
     this.stations = this.stationPrices.ListaEESSPrecio.filter(station => station[this.price] != '' && this.toNumber(station[this.price]) > this.minPriceToShow &&  this.toNumber(station[this.price]) < this.maxPriceToShow);
     if(this.provincePicked != '' && this.provincePicked != undefined){
       this.stations = this.stations.filter(station => station['IDProvincia'] == this.provincePicked);
@@ -136,11 +157,64 @@ export class StationListComponent implements OnInit {
     }else{
       this.invertedSort();
     }
+    this.loading = false;
   }
 
   assignMunicipalities(){
     this.stationService.getMunicipalities(this.provincePicked).subscribe((response: Municipality[]) => {
       response.forEach(municipality => this.municipalities.push(municipality))});
+  }
+
+  openLocation(gasStation: StationPrices){
+    debugger;
+    window.open(`https://www.google.es/maps/dir/${this.userLat},${this.userLng}/${gasStation['Latitud'].replace(",",".")},${gasStation['Longitud (WGS84)'].replace(",",".")}`, "_blank");
+    //`https://www.google.es/maps/@${gasStation['Latitud'].replace(",",".")},${gasStation['Longitud (WGS84)'].replace(",",".")},16z`
+    //`https://www.google.es/maps/dir/${this.userLat},${this.userLng}/'${this.userLat},${this.userLng}'/${gasStation['Latitud'].replace(",",".")},${gasStation['Longitud (WGS84)'].replace(",",".")},14z`
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        if (position) {
+          console.log("Latitude: " + position.coords.latitude +
+            "Longitude: " + position.coords.longitude);
+          this.userLat = position.coords.latitude;
+          this.userLng = position.coords.longitude;
+          console.log(this.userLat);
+          console.log(this.userLng);
+          this.geoLocalized = true;
+        }
+      },
+        (error) => console.log(error));
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  calculateDistance(station: StationPrices): number{
+    // The math module contains a function
+    // named toRadians which converts from
+    // degrees to radians.
+    let userLat = this.userLat * Math.PI / 180;
+    let userLon = this.userLng * Math.PI / 180;
+
+    let stationLon = this.toNumber(station['Longitud (WGS84)']) * Math.PI / 180;
+    let stationLat = this.toNumber(station['Latitud']) * Math.PI / 180;
+
+    // Haversine formula
+    let dlon = userLon - stationLon;
+    let dlat = userLat - stationLat;
+    let a = Math.pow(Math.sin(dlat / 2), 2)
+    + Math.cos(userLat) * Math.cos(stationLat)
+    * Math.pow(Math.sin(dlon / 2),2);
+
+    let c = 2 * Math.asin(Math.sqrt(a));
+
+    // Radius of earth in kilometers.
+    let r = 6371;
+
+    // calculate the result
+    return Math.round((c * r) * 100) / 100;
   }
 
 }
